98b2f7d3191a530f4266544731093127b301f144
VALERIIA SYDORENKO <s30869@pjwstk.edu.pl>
2025-03-07 13:16:44 +0100

    Modyfikacja 3

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("3");
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("2");
